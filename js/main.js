dd453115620eb2b11e8a37f631b02ec344ea8974
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-15 23:44:45 +0300

    23

--- js/main.js
+++ js/main.js
@@ -1719,73 +1719,81 @@
 //   const booksByAuthor = books.filter(book => book.author.includes(AUTHOR));
 //   console.log(booksByAuthor);
 
-// const usersList =
-// [
-//     {
-//       name: "Moore Hensley",
-//       email: "moorehensley@indexia.com",
-//       eyeColor: "blue",
-//       friends: ["Sharron Pace"],
-//       isActive: false,
-//       balance: 2811,
-//       gender: "male"
-//     },
-//     {
-//       name: "Sharlene Bush",
-//       email: "sharlenebush@tubesys.com",
-//       eyeColor: "blue",
-//       friends: ["Briana Decker", "Sharron Pace"],
-//       isActive: true,
-//       balance: 3821,
-//       gender: "female"
-//     },
-//     {
-//       name: "Ross Vazquez",
-//       email: "rossvazquez@xinware.com",
-//       eyeColor: "green",
-//       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//       isActive: false,
-//       balance: 3793,
-//       gender: "male"
-//     },
-//     {
-//       name: "Elma Head",
-//       email: "elmahead@omatom.com",
-//       eyeColor: "green",
-//       friends: ["Goldie Gentry", "Aisha Tran"],
-//       isActive: true,
-//       balance: 2278,
-//       gender: "female"
-//     },
-//     {
-//       name: "Carey Barr",
-//       email: "careybarr@nurali.com",
-//       eyeColor: "blue",
-//       friends: ["Jordan Sampson", "Eddie Strong"],
-//       isActive: true,
-//       balance: 3951,
-//       gender: "male"
-//     },
-//     {
-//       name: "Blackburn Dotson",
-//       email: "blackburndotson@furnigeer.com",
-//       eyeColor: "brown",
-//       friends: ["Jacklyn Lucas", "Linda Chapman"],
-//       isActive: false,
-//       balance: 1498,
-//       gender: "male"
-//     },
-//     {
-//       name: "Sheree Anthony",
-//       email: "shereeanthony@kog.com",
-//       eyeColor: "brown",
-//       friends: ["Goldie Gentry", "Briana Decker"],
-//       isActive: true,
-//       balance: 2764,
-//       gender: "female"
-//     }
-//   ];
+const usersList =
+[
+    {
+      name: "Moore Hensley",
+      email: "moorehensley@indexia.com",
+      eyeColor: "blue",
+      friends: ["Sharron Pace"],
+      isActive: false,
+      balance: 2811,
+      gender: "male",
+      age: 37
+    },
+    {
+      name: "Sharlene Bush",
+      email: "sharlenebush@tubesys.com",
+      eyeColor: "blue",
+      friends: ["Briana Decker", "Sharron Pace"],
+      isActive: true,
+      balance: 3821,
+      gender: "female",
+      age: 34
+    },
+    {
+      name: "Ross Vazquez",
+      email: "rossvazquez@xinware.com",
+      eyeColor: "green",
+      friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+      isActive: false,
+      balance: 3793,
+      gender: "male",
+      age: 24
+    },
+    {
+      name: "Elma Head",
+      email: "elmahead@omatom.com",
+      eyeColor: "green",
+      friends: ["Goldie Gentry", "Aisha Tran"],
+      isActive: true,
+      balance: 2278,
+      gender: "female",
+      age: 21
+    },
+    {
+      name: "Carey Barr",
+      email: "careybarr@nurali.com",
+      eyeColor: "blue",
+      friends: ["Jordan Sampson", "Eddie Strong"],
+      isActive: true,
+      balance: 3951,
+      gender: "male",
+      age: 27
+    },
+    {
+      name: "Blackburn Dotson",
+      email: "blackburndotson@furnigeer.com",
+      eyeColor: "brown",
+      friends: ["Jacklyn Lucas", "Linda Chapman"],
+      isActive: false,
+      balance: 1498,
+      gender: "male",
+      age: 38
+    },
+    {
+      name: "Sheree Anthony",
+      email: "shereeanthony@kog.com",
+      eyeColor: "brown",
+      friends: ["Goldie Gentry", "Briana Decker"],
+      isActive: true,
+      balance: 2764,
+      gender: "female",
+      age: 39
+    }
+  ];
 //   const getUsersWithEyeColor = (users, color) => users.filter(user => user.eyeColor === color)
 //   console.log(getUsersWithEyeColor(usersList, 'blue'));
   
-
+const getUsersWithAge = (users, minAge, maxAge) => users.filter(user => (user.age > minAge && user.age < maxAge))
+console.log(getUsersWithAge(usersList, 20, 30));
