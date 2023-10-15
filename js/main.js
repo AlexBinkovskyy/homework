54deffa84916339acab1a7841502f247a556047a
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-15 20:27:28 +0300

    7

--- js/main.js
+++ js/main.js
@@ -1431,17 +1431,16 @@
 //   console.log(filterArray([12, 24, 8, 41, 76], 38));
 //   console.log(filterArray([12, 24, 8, 41, 76], 20));
 
-function getCommonElements(firstArray, secondArray) {
-    const commonElements = [];
-    // Change code below this line
-  
-    for (let i = 0; i < firstArray.length; i += 1) {
-      if (secondArray.includes(firstArray[i])) {
-        commonElements.push(firstArray[i]);
-      }
-    }
-  
-    return commonElements;
-    // Change code above this line
-  }
+// function getCommonElements(firstArray, secondArray) {
+//     const commonElements = [];
+
+//   firstArray.forEach(item => { if (secondArray.includes(item)) {commonElements.push(item)}})
+//       return commonElements;
+
+//   }
+//   console.log(getCommonElements([1, 2, 3], [2, 4]));
+//   console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+//   console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+//   console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
+//   console.log(getCommonElements([1, 2, 3], [10, 20, 30]));
   
