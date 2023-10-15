742b3da956c24f514ffcfa861cd18d85e191f0c1
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-15 19:53:18 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1414,3 +1414,19 @@
 // console.log(calculateTotalPrice([12, 85, 37, 4]));
 // console.log(calculateTotalPrice([164, 48, 291]));
 // console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
+
+// function filterArray(numbers, value) {
+//     const filteredNumbers = [];
+//     // Change code below this line
+  
+//     numbers.forEach(number => { number > value ? filteredNumbers.push(number) : number});
+  
+//     // Change code above this line
+//     return filteredNumbers;
+//   }
+
+//   console.log(filterArray([1, 2, 3, 4, 5], 3));
+//   console.log(filterArray([1, 2, 3, 4, 5], 4));
+//   console.log(filterArray([1, 2, 3, 4, 5], 5));
+//   console.log(filterArray([12, 24, 8, 41, 76], 38));
+//   console.log(filterArray([12, 24, 8, 41, 76], 20));
