3f57c2f5f74ef738a7ee7115d3d530862f1c2626
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-15 23:07:02 +0300

    19

--- js/main.js
+++ js/main.js
@@ -1649,3 +1649,9 @@
 //   console.log(getUserEmails(usersList));
 
 
+// const numbers = [17, 24, 82, 61, 36, 18, 47, 52, 73];
+// const evenNumbers = numbers.filter(number => number % 2 === 0);
+// const oddNumbers = numbers.filter(number => number % 2 !== 0);
+// console.log(evenNumbers);
+// console.log(oddNumbers);
+
