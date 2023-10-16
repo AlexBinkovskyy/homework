ec47a27556efd99c951b809c30a9f260f3184224
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-16 18:22:32 +0300

    35

--- js/main.js
+++ js/main.js
@@ -1873,3 +1873,17 @@ const usersList =
 // console.log(averagePlayTime);
 
 
+const players = [
+  { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+  { name: "Poly", playtime: 469, gamesPlayed: 2 },
+  { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+  { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+];
+
+
+const totalAveragePlaytimePerGame = players.reduce((total, player) => {return total = total + (player.playtime / player.gamesPlayed)}, 0)
+console.log(totalAveragePlaytimePerGame);
+317.5
+234,5
+230
+241
