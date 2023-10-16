f5843f5a8b7fa842799071dfa302525885189ac4
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-16 18:35:27 +0300

    38

--- js/main.js
+++ js/main.js
@@ -1886,3 +1886,15 @@ const usersList =
 // console.log(totalAveragePlaytimePerGame);
 
 
+// const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// const authors = [
+//   "Tanith Lee",
+//   "Bernard Cornwell",
+//   "Robert Sheckley",
+//   "Fyodor Dostoevsky",
+// ];
+// const ascendingReleaseDates = [...releaseDates].sort();
+// const alphabeticalAuthors = [...authors].sort();
+// console.log(ascendingReleaseDates);
+// console.log(alphabeticalAuthors);
+
