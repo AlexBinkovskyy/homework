b4d551532c0d36ba4b18798ff1dc40bf0dd02b09
AlexBinkovskyy <alex_bin@ukr.net>
2023-10-16 18:40:35 +0300

    39

--- js/main.js
+++ js/main.js
@@ -1898,3 +1898,10 @@ const usersList =
 // console.log(ascendingReleaseDates);
 // console.log(alphabeticalAuthors);
 
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+
+const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
+const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+console.log(ascendingReleaseDates);
+console.log(descendingReleaseDates);
